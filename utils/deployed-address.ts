16ceeec288b284acd24c36192e543c9895b1b030
root <root@DESKTOP-4400F6U>
2024-08-18 12:30:11 +0100

    fix: Fixed the upgrade functionality

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xbe1e5366Ca6dCEcAa5b6603e6c81e6E9d5a34D5d'
-export const ProxyAdmin = '0x600e824d911bE1a3bfe74C6edB6D20d8fFbfD217'
-export const SWTRImplementation = '0x7902B199A487869B167d0eA34084402AEd003C7A'
+export const SWTRProxy = '0x3B4D91dE19a5B0f3DBcb4312D3703B5eA70DeC56'
+export const ProxyAdmin = '0xe02CE804AE63f150c6BB9E0B8C8f50cbe55753da'
+export const SWTRImplementation = '0x4631A08aA60D3B872C532f0816C72C4cc062a502'
     
